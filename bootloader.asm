9c93b9a6983abdf515d4ad7924f16044b088da8c
Ahmed Rashid <ahmedsayidrashid@gmail.com>
2025-01-29 00:18:51 -0500

    First commit

---
+++ bootloader.asm
@@ -0,0 +1,26 @@
+; bootloader.asm
+[org 0x7C00]           ; This specifies that the bootloader will load at 0x7C00 in memory.
+
+; Set up the video mode
+mov ah, 0x0E           ; Teletype output (prints a character)
+mov al, 'H'            ; The character to print
+int 0x10               ; BIOS video interrupt
+mov al, 'e'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'l'
+int 0x10
+mov al, 'o'
+int 0x10
+mov al, pc 
+int 0x10
+
+; Infinite loop (so the bootloader doesn’t exit)
+hang:
+    nop
+    jmp hang            ; Loop forever
+    
+; The bootloader must end with this signature:
+times 510 - ($ - $$) db 0 ; Pad the rest of the boot sector with zeros
+dw 0xAA55               ; Boot sector signature (this tells the BIOS this is a valid bootloader)
